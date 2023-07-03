ac94026c702c4bc8d695705cdf41b9db568e4aee
Jorman Urbina <jormanurbina1@gmail.com>
2023-07-03 19:17:46 -0300

    Se agrego condicional para agregar el oculto al formulario y quitarlo del elemento html con el id "exito" , tambien se agrego un setTimeout para que luego de 5 segundos de que el formulario no tuviera ningun error despues de haberlo enviado te lleve a la pagina whishlist.html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 function validacionNombre(nombre) {
   if (nombre.length === 0) {
     return "este campo debe tener al menos 1 caracter";
@@ -29,51 +28,68 @@ function validacionDescripcionRegalo(descripcionRegalo) {
   }
 }
 function validarFormulario(event) {
-  
-  
-  const $form =document.querySelector("#carta-a-santa");
-  
-  const nombre= $form.nombre.value;
-  const ciudad=$form.ciudad.value;
-  const descripcionRegalo=$form["descripcion-regalo"].value;
-  
-  const errorNombre=validacionNombre(nombre);
-  const errorCiudad=validacionCiudad(ciudad);
-  const errorDescripcionRegalo=validacionDescripcionRegalo(descripcionRegalo);
+  const $form = document.querySelector("#carta-a-santa");
 
-  const errores={
-    nombre:errorNombre,
-    ciudad:errorCiudad,
-    descripcionRegalo:errorDescripcionRegalo
+  const nombre = $form.nombre.value;
+  const ciudad = $form.ciudad.value;
+  const descripcionRegalo = $form["descripcion-regalo"].value;
+
+  const errorNombre = validacionNombre(nombre);
+  const errorCiudad = validacionCiudad(ciudad);
+  const errorDescripcionRegalo = validacionDescripcionRegalo(descripcionRegalo);
+
+  const errores = {
+    nombre: errorNombre,
+    ciudad: errorCiudad,
+    "descripcion-regalo": errorDescripcionRegalo,
   };
-  manejarErrores(errores)
-  
+  const esExito = manejarErrores(errores) === 0;
+  if (esExito) {
+    $form.className = "oculto";
+    document.querySelector("#exito").className = "";
+    setTimeout(function () {
+      window.location.href = "wishlist.html";
+    }, 5000);
+  }
+
   event.preventDefault();
 }
 
 function manejarErrores(errores) {
-  
-  errorNombre=errores.nombre;
-  errorCiudad=errores.ciudad;
-  errorDescripcionRegalo=errores.descripcionRegalo
+  const keys = Object.keys(errores);
+  const $errores = document.querySelector("#errores");
+  let cantidadErrores = 0;
+  keys.forEach(function (key) {
+    const error = errores[key];
+    let $listaDeErrores = document.querySelectorAll("#errores li");
 
-  if (errorNombre) {
-    $form.nombre.className="error"
-    
-  } else {
-    $form.nombre.className=""
-  }
-  if (errorCiudad) {
-    $form.ciudad.className="error"   
-  } else {
-    $form.ciudad.className=""
-  }
-  if (errorDescripcionRegalo) {
-    $form["descripcion-regalo"].className="error"
-  } else {
-    $form["descripcion-regalo"].className=""
-  }
+    if (error) {
+      $form[key].className = "error";
+
+      for (let i = 0; i < $listaDeErrores.length; i++) {
+        if (error === $listaDeErrores[i].innerText) {
+          $listaDeErrores[i].remove();
+        }
+      }
+
+      cantidadErrores++;
+
+      const $error = document.createElement("li");
+      $error.innerText = error;
+      $error.id = key;
+      $errores.appendChild($error);
+    } else {
+      $form[key].className = "";
+      for (let i = 0; i < $listaDeErrores.length; i++) {
+        if (key === $listaDeErrores[i].id) {
+          $listaDeErrores[i].remove();
+        }
+      }
+    }
+  });
+  return cantidadErrores;
 }
 const $form = document.querySelector("#carta-a-santa");
 
 $form.onsubmit = validarFormulario;
+//tarea: borrar la lista de errores en cada submit 1:53:00, bonus, borrar el campo adecuado en el else
